593da07d194d1d0f2a22477c8b43619f6932ec9a
kyeongseoJ <86696753+kyeongseoJ@users.noreply.github.com>
2022-04-12 15:03:42 +0900

    player movement cs
    20220412

--- ZombieS/Assets/02. Scripts/PlayerMovement.cs
+++ ZombieS/Assets/02. Scripts/PlayerMovement.cs
@@ -13,8 +13,11 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private Rigidbody playerRigidbody; // 플레이어 캐릭터의 리지드바디
     private Animator playerAnimator; // 플레이어 캐릭터의 애니메이터
 
+    // 시간확인(test)
+    float time2 = 0f;
     void Start()
     {
+
         //사용할 컴포넌트들의 참조 가져오기
         playerInput = GetComponent<PlayerInput>();
         playerRigidbody = GetComponent<Rigidbody>();
@@ -48,7 +51,15 @@ public class PlayerMovement : MonoBehaviour
         // 상대적으로 회전할 수치 계산
         float turn = playerInput.rotate * rotateSpped * Time.deltaTime;
         // 리지드바디를 이용해 게임 오브젝트 회전 변경
-        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0);
+        playerRigidbody.rotation = playerRigidbody.rotation * Quaternion.Euler(0, turn, 0f);
     }
 
+    private void Update()
+    {
+        // 시간확인하는 코드 (test)
+        Debug.Log("Time : " + (int)Time.time);
+        time2 += Time.deltaTime;
+        Debug.Log("deltaTime : " + (int)time2);
+        Debug.Log("FixedUpdate : " + (float)Time.fixedDeltaTime);
+    }
 }
