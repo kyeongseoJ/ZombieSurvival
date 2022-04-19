68a9e6a81510072e8c9444ad52f60b91f8fed153
kyeongseoJ <86696753+kyeongseoJ@users.noreply.github.com>
2022-04-19 15:36:37 +0900

    out and interface

--- ZombieS/Assets/02. Scripts/GunData.cs
+++ ZombieS/Assets/02. Scripts/GunData.cs
@@ -10,14 +10,26 @@ public class GunData : ScriptableObject
     public AudioClip shotClip; // 발사 소리
     public AudioClip reloadClip; // 재장전 소리
 
-    // 
+    
     public float damage = 25; // 공격력
 
-    public int startAmmoRemain = 100; // 처음에 주어질 전체 탄알
-    public int magCapacity = 25; // 탄창용량
+    /// <summary>
+    /// 처음에 주어질 전체 탄알
+    /// </summary>
+    public int startAmmoRemain = 100;
+    /// <summary>
+    /// 탄창용량
+    /// </summary>
+    public int magCapacity = 25; 
 
-    public float timeBetFire = 0.12f; // 탄알 발사 사이의 간격
-    public float reloadTime = 1.8f; // 재장전 소요 시간
+    /// <summary>
+    /// 탄알 발사 사이의 간격
+    /// </summary>
+    public float timeBetFire = 0.12f;
+    /// <summary>
+    /// 재장전 소요 시간
+    /// </summary>
+    public float reloadTime = 1.8f; 
 
 
 }
