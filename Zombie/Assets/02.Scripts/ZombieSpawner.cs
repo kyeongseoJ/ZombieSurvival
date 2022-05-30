b8b74614a586457c81aabd1a04ddadace8edf013
kyeongseoJ <86696753+kyeongseoJ@users.noreply.github.com>
2022-05-30 16:37:12 +0900

    zombie spwan , game manager setting

--- Zombie/Assets/02.Scripts/ZombieSpawner.cs
+++ Zombie/Assets/02.Scripts/ZombieSpawner.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 public class ZombieSpawner : MonoBehaviour
 {
     public Zombie zombiePrefab; // 생성할 원본 좀비 프리팹
+    // public Zombie[] zombiePrefabs; // 좀비 종류가 여러 개일 경우
 
     public ZombieData[] zombieDatas; // 사용할 좀비 셋업 데이터
     public Transform[] spawnPoints; // 좀비 AI 생성할 위치
@@ -13,7 +14,8 @@ public class ZombieSpawner : MonoBehaviour
     private List<Zombie> zombies = new List<Zombie>(); // 생성된 좀비를 담는 리스트
     private int wave; // 현재 웨이브
 
-    private void Update() {
+    private void Update() 
+    {
         // 게임오버 상태일 때는 생성하지 않음
         if(GameManager.instance != null && GameManager.instance.isGameover)
         {
@@ -23,20 +25,22 @@ public class ZombieSpawner : MonoBehaviour
         // 좀비를 모두 물리친 경우 다름 스폰 실행
         if(zombies.Count <= 0)
         {
-            spawnWave();
+            SpawnWave();
         }
         // UI 갱신
         UpdateUI();
     }
 
     // 웨이브 정보를 UI로 갱신
-    private void UpdateUI(){
+    private void UpdateUI()
+    {
         // 현재 웨이브와 남은 좀비 수 표시
         UIManager.instance.UpdateWaveText(wave, zombies.Count);
     }
 
     // 현재 웨이브에 맞춰 좀비 생성
-    private void spawnWave(){
+    private void SpawnWave()
+    {
 
         // 웨이브 1 증가 : 게임 시작 시 웨이브가 0 이라서 1 증가 시켜주고 시작
         wave++;
@@ -52,12 +56,16 @@ public class ZombieSpawner : MonoBehaviour
     }
 
     // 좀비를 생성하고 좀비에 추적할 대상 할당
-    private void CreateZombie(){
+    private void CreateZombie()
+    {
         // 사용할 좀비 데이터 랜덤으로 결정
         ZombieData zombieData= zombieDatas[Random.Range(0, zombieDatas.Length)];
 
         // 생성할 위치를 랜덤으로 결정
         Transform spawnPoint = spawnPoints[Random.Range(0, spawnPoints.Length)];
+
+        // 좀비가 여러개일 경우 위와 동일하게 받아와서 쓰면 된다.
+        // Zombie zombieObject = zombiePrefabs[Random.Range(0,zombiePrefabs.Length)];
 
         // 좀비 프리팹으로부터 좀비 생성
         Zombie zombie = Instantiate(zombiePrefab, spawnPoint.position, spawnPoint.rotation);
